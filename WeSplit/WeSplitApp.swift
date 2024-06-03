f975754b3e587eda12dd846ab14a36362e8e28ea
ysmaraujo <yasminnaraujo@hotmail.com>
2024-06-03 15:58:22 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Yasmin araujo on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
